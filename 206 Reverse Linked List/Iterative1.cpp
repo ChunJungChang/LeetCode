8e08fc62ca3fb189222eb61b29bc198d94431d5d
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-02-04 19:07:50 +0800

    Create Iterative1.cpp

---
+++ 206 Reverse Linked List/Iterative1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode *node_ptr_1 = head;
+        ListNode *node_ptr_2 = head->next;
+        ListNode *node_ptr_3 = head->next->next;
+        
+        node_ptr_1->next = NULL;
+        while (node_ptr_3 != NULL) {
+            node_ptr_2->next = node_ptr_1;
+            node_ptr_1 = node_ptr_2;
+            node_ptr_2 = node_ptr_3;
+            node_ptr_3 = node_ptr_3->next;
+        }
+        node_ptr_2->next = node_ptr_1;
+        
+        return node_ptr_2;
+    }
+};
