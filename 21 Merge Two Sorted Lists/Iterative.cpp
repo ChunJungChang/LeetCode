a58a74aa13a4547a7e21bb98911309f3637d2e76
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-02-04 18:12:30 +0800

    Create Iterative.cpp

---
+++ 21 Merge Two Sorted Lists/Iterative.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL || l2 == NULL)
+            return (l1 != NULL) ? l1 : l2;
+        
+        ListNode *head = NULL, *now = NULL;
+        if (l1->val < l2->val) {
+            head = now = l1;
+            l1 = l1->next;
+        }
+        else {
+            head = now = l2;
+            l2 = l2->next;
+        }
+
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val <= l2->val) {
+                now->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                now->next = l2;
+                l2 = l2->next;
+            }
+            now = now->next;
+        }
+        now->next = (l1 == NULL) ? l2 : l1;
+        
+        return head;
+    }
+};
