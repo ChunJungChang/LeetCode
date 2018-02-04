5f82a289a41633595076a1411721961db59bfac3
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-02-04 18:28:23 +0800

    Create Iterative.cpp

---
+++ 83 Remove Duplicates from Sorted List/Iterative.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *tmp = head;
+        
+        if (head == NULL)
+            return head;
+        while (tmp->next) {
+            if (tmp->val == tmp->next->val) {
+                tmp->next = tmp->next->next;
+            }
+            else
+                tmp = tmp->next;
+        }
+        
+        return head;
+    }
+};
