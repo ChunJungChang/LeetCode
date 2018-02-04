b0eba5e217a7caf09e30bd57fd93f9beb93adc18
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-02-04 17:36:06 +0800

    Create DFS.c

---
+++ 104 Maximum Depth of Binary Tree/DFS.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    int dep = 0;
+    
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int Ldep = 1 + maxDepth(root->left);
+        int Rdep = 1 + maxDepth(root->right);
+        
+        if (Ldep > Rdep)
+            dep = Ldep;
+        else
+            dep = Rdep;
+            
+        return dep;
+    }
+};
