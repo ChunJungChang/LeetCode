717ccb5abd3a86357ac92b560c1840f2ff9b6eb0
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-02-04 18:27:32 +0800

    Create Recursive.cpp

---
+++ 83 Remove Duplicates from Sorted List/Recursive.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *tmp;
+        
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        
+        tmp = deleteDuplicates(head->next);
+        
+        if (head->val == tmp->val) {
+            return tmp;
+        } else {
+            head->next = tmp;
+            return head;
+        }
+    }
+};
