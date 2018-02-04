6226ae058211451ad5f50713f1d745b3410cd447
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-02-04 18:14:40 +0800

    Create Iterative.cpp

---
+++ 24 Swap Nodes in Pairs/Iterative.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        
+        ListNode *fir = head;
+        ListNode *sec = head->next;
+        ListNode *res = head->next->next;
+        head = head->next;
+        
+        // swap the first and second node
+        fir->next = res;
+        sec->next = fir;
+        
+        while (res != NULL && res->next != NULL) {
+            ListNode *pre = fir;
+            fir = res;
+            sec = res->next;
+            res = res->next->next;
+            
+            fir->next = res;
+            sec->next = fir;
+            pre->next = sec;
+            
+            if (res == NULL || res->next == NULL)
+                break;
+        }
+        
+        return head;
+    }
+};
