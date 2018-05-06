0c58e6324469ad8df71c5d54c034b26c74a77e92
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2018-05-06 21:32:12 +0800

    Update DiscussSolution.java

--- Easy/326 Power of Three/DiscussSolution.java
+++ Easy/326 Power of Three/DiscussSolution.java
@@ -1,4 +1,4 @@
-/**  The idea is to convert the original number into radix-3 format
+/** The idea is to convert the original number into radix-3 format
  *  and check if it is of format 10* where 0* means k zeros, k >= 0
  */
  public boolean isPowerOfThree(int n) {
