025f911f2bbd98aa36960c6e9b9de855936121e2
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2021-10-22 10:40:04 +0800

    Create MySolution_DFS.c

---
+++ Easy/111 Minimum Depth of Binary Tree/MySolution_DFS.c
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+int minDepth(struct TreeNode* root){
+    int leftDepth = 0, rightDepth = 0;
+    
+    if (!root) {
+        return 0;
+    }
+    
+    leftDepth += minDepth(root->left);
+    rightDepth += minDepth(root->right);
+    
+    if (leftDepth == 0 && rightDepth == 0) {
+        return 1;
+    } else if (rightDepth == 0 && leftDepth > 0) {
+        return leftDepth + 1;
+    } else if (leftDepth == 0 && rightDepth > 0) {
+        return rightDepth + 1;
+    } else if (leftDepth < rightDepth) {
+        return leftDepth + 1;
+    } else {
+        return rightDepth + 1;
+    }
+}
