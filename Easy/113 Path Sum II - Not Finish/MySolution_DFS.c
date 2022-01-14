f01dce73202af43b53661d67a9437ee313edf158
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2022-01-14 12:49:08 +0800

    Update MySolution_DFS.c

--- Easy/113 Path Sum II - Not Finish/MySolution_DFS.c
+++ Easy/113 Path Sum II - Not Finish/MySolution_DFS.c
@@ -16,48 +16,98 @@ int path_record[5000];
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** pathSum(struct TreeNode* root, int targetSum, int* returnSize, int** returnColumnSizes) {
-    int i = 0;
-    int *column = NULL, **res_left = NULL, **res_right = NULL, **res= NULL;
-
+    int i = 0, j = 0, returnSize_left = 0, returnSize_right = 0, size = 0;
+    int *returnColumnSizes_left = NULL, *returnColumnSizes_right = NULL,
+        **res_left = NULL, **res_right = NULL, **res= NULL;
+    
     depth += 1;
+    *returnSize = size;
     
     if (!root) {
         depth -= 1;
-        
-        *returnSize = 0;
-        *returnColumnSizes = NULL;
-        
-        return res;
+
+        return NULL;
     }
     
     path_record[depth - 1] = root->val;
-    
     targetSum -= root->val;
-    
-    if (targetSum == 0 && !root->left && !root->right) {
-        returnSize = calloc(1, sizeof(int));
-        *returnSize = 1;
 
-        returnColumnSizes = (int**)calloc(1, sizeof(int*));
+    if (targetSum == 0 && !(root->left) && !(root->right)) {
+        size = 1;
+
         *returnColumnSizes = (int*)calloc(1, sizeof(int));
-        *returnColumnSizes = depth;
+        **returnColumnSizes = depth;
 
         res = (int**)calloc(1, sizeof(int*));
         *res = (int*)calloc(depth, sizeof(int));
         for (i = 0; i < depth; i++) {
             res[0][i] = path_record[i];
-            printf("res[0][%d]: %d \n", i, res[0][i]);
+            /*
+            printf("Leaf, res[0][%d]: %d \n", i, res[0][i]);
+            */
         }
-
-        depth -= 1;
-        return res;
     } else {
-        res_left = pathSum(root->left, targetSum, returnSize, returnColumnSizes);
-        res_right = pathSum(root->right, targetSum, returnSize, returnColumnSizes);
+        if (root->left) {
+            res_left = pathSum(root->left, targetSum, &returnSize_left, &returnColumnSizes_left);
+        }
+        size += returnSize_left;
+        if (root->right) {
+            res_right = pathSum(root->right, targetSum, &returnSize_right, &returnColumnSizes_right);
+        }
+        size += returnSize_right;
         
+        /*
+        printf("Non-Leaf, root->val: %d\n", root->val);
+        */
         
+        if (size > 0) {
+            *returnColumnSizes = (int*)calloc(size, sizeof(int));
+            res = (int**)calloc(size, sizeof(int*));
+
+            for (i = 0; i < returnSize_left; i++) {
+                (*returnColumnSizes)[i] = returnColumnSizes_left[i];
+                res[i] = (int*)calloc(returnColumnSizes_left[i], sizeof(int));
+                for (j = 0; j < returnColumnSizes_left[i]; j++) {
+                    res[i][j] = res_left[i][j];
+                    /*
+                    printf("Non-Leaf left, res[%d][%d]: %d \n", i, j, res[i][j]);
+                    */
+                }
+            }
+            for (i = 0; i < returnSize_right; i++) {
+                (*returnColumnSizes)[returnSize_left + i] = returnColumnSizes_right[i];
+                res[returnSize_left + i] = (int*)calloc(returnColumnSizes_right[i], sizeof(int));
+                for (j = 0; j < returnColumnSizes_right[i]; j++) {
+                    res[returnSize_left + i][j] = res_right[i][j];
+                    /*
+                    printf("Non-Leaf right, res[%d][%d]: %d \n", returnSize_left + i, j, res[returnSize_left + i][j]);
+                    */
+                }
+            }
+            
+            if (returnColumnSizes_left) {
+                free(returnColumnSizes_left);
+            }
+            if (returnSize_left) {
+                for (i = 0; i < returnSize_left; i++) {
+                    free(res_left[i]);
+                }
+            }
+            if (returnColumnSizes_right) {
+                free(returnColumnSizes_right);
+            }
+            if (returnSize_right) {
+                for (i = 0; i < returnSize_right; i++) {
+                    free(res_right[i]);
+                }
+            }
+        }
     }
     
     depth -= 1;
-    return returnColumnSizes;
+    *returnSize = size;
+    /*
+    printf("===============\n");
+    */
+    return res;
 }
