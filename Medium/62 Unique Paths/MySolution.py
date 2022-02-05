61abca3b68d800a1ccbd120ce3840735a031b826
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2022-02-05 12:15:47 +0800

    Create MySolution.py

---
+++ Medium/62 Unique Paths/MySolution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        stepArr = [[0 for i in range(n)] for j in range(m)];
+        
+        for i in range(m):
+            stepArr[i][0] = 1
+        for j in range(n):
+            stepArr[0][j] = 1
+            
+        for i in range(1, m):
+            for j in range(1, n):
+                stepArr[i][j] = stepArr[i - 1][j] + stepArr[i][j - 1]
+        
+        return stepArr[m - 1][n - 1]
