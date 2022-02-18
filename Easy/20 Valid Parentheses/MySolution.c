93f5b192381e4577265ca43736efe28565bae56e
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2022-02-19 01:29:22 +0800

    Update MySolution.c

--- Easy/20 Valid Parentheses/MySolution.c
+++ Easy/20 Valid Parentheses/MySolution.c
@@ -1,34 +1,42 @@
 
 
 bool isValid(char * s){
-    char brackets_record[10000] = {0};
-    int len = 0, i = 0, j = 0;
+    char *brackets_record = NULL;
+    int s_len = 0, i = 0, j = 0;
+    bool ret = true;
     
-    len = strlen(s);
+    s_len = strlen(s);
+    if (s_len == 0) {
+        goto exit;
+    }
+    brackets_record = malloc(s_len * sizeof(char));
     
-    for (i = 0; i < len; i++) {
-        if (s[i] == '(' ||
-            s[i] == '{' ||
-            s[i] == '[') {
+    for (i = 0; i < s_len; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
             brackets_record[j++] = s[i];
-        } else if (s[i] == ')') {
-            if (--j < 0 || brackets_record[j] != '(') {
-                return false;
+        } else {
+            if (--j < 0) {
+                ret = false;
+                goto exit;
             }
-        } else if (s[i] == '}') {
-            if (--j < 0 || brackets_record[j] != '{') {
-                return false;
-            }
-        } else if (s[i] == ']') {
-            if (--j < 0 || brackets_record[j] != '[') {
-                return false;
+            
+            if (s[i] == ')' && brackets_record[j] != '(') {
+                ret =  false;
+                goto exit;
+            } else if (s[i] == '}' && brackets_record[j] != '{') {
+                ret =  false;
+                goto exit;
+            } else if (s[i] == ']' && brackets_record[j] != '[') {
+                ret = false;
+                goto exit;
             }
         }
     }
     
     if (j != 0) {
-        return false;
+        ret = false;
     }
-    
-    return true;
+exit:
+    free(brackets_record);
+    return ret;
 }
