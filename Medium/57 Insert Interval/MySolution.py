d1105b6e8bc47940275f676319278141c6cc0772
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2022-03-22 23:14:36 +0800

    Update MySolution.py

--- Medium/57 Insert Interval/MySolution.py
+++ Medium/57 Insert Interval/MySolution.py
@@ -1,31 +1,31 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        newStartPlace = -1
-        newEndPlace = -1
+        newStartVal = -1
+        newEndVal = -1
         ret = []
         
         for i in range(len(intervals)):
             if intervals[i][0] <= newInterval[0] and newInterval[0] <= intervals[i][1]:
-                newStartPlace = intervals[i][0]
+                newStartVal = intervals[i][0]
                 break
         
         for i in range(len(intervals)):
             if intervals[i][0] <= newInterval[1] and newInterval[1] <= intervals[i][1]:
-                newEndPlace = intervals[i][1]
+                newEndVal = intervals[i][1]
                 break
             if intervals[i][1] < newInterval[1] and i + 1 < len(intervals) and newInterval[1] < intervals[i + 1][0]:
-                newEndPlace = newInterval[1]
+                newEndVal = newInterval[1]
                 break
         
         
-        if newStartPlace == -1:
-            newStartPlace = newInterval[0]
-        if newEndPlace == -1:
-            newEndPlace = newInterval[1]
+        if newStartVal == -1:
+            newStartVal = newInterval[0]
+        if newEndVal == -1:
+            newEndVal = newInterval[1]
         for i in range(len(intervals)):
-            if not (newStartPlace <= intervals[i][0] and intervals[i][1] <= newEndPlace):
+            if not (newStartVal <= intervals[i][0] and intervals[i][1] <= newEndVal):
                 ret.append(intervals[i])
-        ret.append([newStartPlace, newEndPlace])
+        ret.append([newStartVal, newEndVal])
         ret.sort()
         
         return ret
