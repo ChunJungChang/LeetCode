0cdd0f760c0bcd67628695147311b048d41b1aa9
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2022-04-12 23:12:35 +0800

    Create KadaneAlgorithm.c

---
+++ Easy/53 Maximum Subarray/KadaneAlgorithm.c
@@ -0,0 +1,28 @@
+
+
+int maxSubArray(int* nums, int numsSize){
+    int curSum = nums[0], globalSum = nums[0], i = 0;
+    
+    if (!nums) {
+        globalSum = 0;
+        return globalSum;
+    }
+    if (numsSize == 1) {
+        globalSum = nums[0];
+        return globalSum;
+    }
+    
+    for (i = 1; i < numsSize; i++) {
+        if (curSum + nums[i] > nums[i]) {
+            curSum += nums[i];
+        } else {
+            curSum = nums[i];
+        }
+        
+        if (globalSum < curSum) {
+            globalSum = curSum;
+        }
+    }
+    
+    return globalSum;
+}
