07df9f9de5937391476c0e5ea2432fe9c305d78a
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2023-09-14 15:50:30 +0800

    Create DiscussSolution_Inorder.c

---
+++ Medium/98 Validate Binary Search Tree/DiscussSolution_Inorder.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* prev = NULL;
+
+bool isValidBSTHelper(struct TreeNode* root){
+    if (!root) {
+        return true;
+    }
+
+    if (isValidBSTHelper(root->left) == false) {
+        return false;
+    }
+
+    if (prev && root->val <= prev->val) {
+        return false;
+    }
+
+    prev = root;
+
+    return isValidBSTHelper(root->right);
+}
+
+bool isValidBST(struct TreeNode* root){
+    bool ret = false;
+
+    ret =  isValidBSTHelper(root);
+    
+    prev = NULL;
+
+    return ret;
+}
