5e7d728863feb0d9397f273898806b2723e00955
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2023-09-20 21:53:43 +0800

    Create MySolution_DFS.c

---
+++ Medium/230 Kth Smallest Element in a BST/MySolution_DFS.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int inorderTraversal(struct TreeNode *root, int k, int *nodeCnt) {
+    int leftCnt = 0, rightCnt = 0, cnt = 0, ret = -1;
+
+    if (!root) {
+        return -1; /* Initial value, 0 <= root->val <=10^4 */
+    }
+
+    ret = inorderTraversal(root->left, k, nodeCnt);
+    *nodeCnt += 1; /* Count current node */
+    if (ret > -1) {
+        /* Find kth node, do nothing */
+    } else if (*nodeCnt == k) {
+        ret = root->val;
+    } else {
+        ret = inorderTraversal(root->right, k, nodeCnt);
+    }
+
+    return ret;
+}
+
+int kthSmallest(struct TreeNode* root, int k){
+    int nodeCnt = 0, res = 0;
+
+    res = inorderTraversal(root, k, &nodeCnt);
+
+    return res;
+}
