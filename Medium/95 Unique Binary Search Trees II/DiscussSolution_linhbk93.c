b8c9cf3b711e3bdb5324d846d5859a1249dc96d1
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2024-03-11 21:40:49 +0800

    Update DiscussSolution_linhbk93.c

--- Medium/95 Unique Binary Search Trees II/DiscussSolution_linhbk93.c
+++ Medium/95 Unique Binary Search Trees II/DiscussSolution_linhbk93.c
@@ -10,6 +10,45 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
+/**
+ * 96. Unique Binary Search Trees
+ */
+int numTrees(int n) {
+    int *arr = NULL, ret = 0, i = 0, j = 0;
+
+    if (n == 0) {
+        return ret;
+    }
+
+    if (n > 0) {
+        arr = (int*)calloc(n + 1, sizeof(int));
+    }
+
+    arr[0] = 1;
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < i; j ++) {
+            /**
+              * arr[j]: left subtree.
+              * arr[i - j - 1]: right subtree, 1 means root.
+              */
+            arr[i] += arr[j] * arr[i - j - 1];
+            /*
+            printf("arr[%d] += arr[%d] * arr[%d]: ", i, j, i - j - 1);
+            printf("%d += %d * %d\n", arr[i], arr[j], arr[i - j - 1]);
+            */
+        }
+    }
+
+    ret = arr[n];
+
+    if (arr) {
+        free(arr);
+        arr = NULL;
+    }
+
+    return ret;
+}
+
 struct TreeNode** createTrees(int low, int high, int *size) {
     struct TreeNode **ret = NULL;
 
@@ -21,10 +60,7 @@ struct TreeNode** createTrees(int low, int high, int *size) {
         return ret;
     }
 
-    /**
-     * 1 <= n <= 8, the maximum number of nodes is 1430 when n = 8.
-     */
-    ret = (struct TreeNode**)malloc(1430 * sizeof(struct TreeNode*));
+    ret = (struct TreeNode**)malloc(numTrees(high - low + 1) * sizeof(struct TreeNode*));
     *size = 0;
 
     for (int i = low; i <= high; i++) {
