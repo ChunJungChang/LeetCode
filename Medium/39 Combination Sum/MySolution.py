a2b482a712a8ad12b3f9a38a6b7098e0089fc030
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2024-12-26 17:35:13 +0800

    Update MySolution.py

--- Medium/39 Combination Sum/MySolution.py
+++ Medium/39 Combination Sum/MySolution.py
@@ -1,26 +1,29 @@
 class Solution:
     def combinationSumHelp(self, candidates: List[int], target: int, idx: int) -> List[List[int]]:
-        resTake, resNotTake = [], []
+        res, resTake, resNotTake = [], [], []
         ret = []
 
+        #  Just add the element to result and slice list if the element of the list is equal to target.
         if target in candidates:
             ret.append([target])
-            candidates[:-1]
-
-        # Take candidates[idx].
-        if target - candidates[idx] >= candidates[0]:
-            resTake = self.combinationSumHelp(candidates, target - candidates[idx], idx)
-            for item in resTake:
-                item.insert(0, candidates[idx])
-            for item in resTake:
+            candidates = candidates[:candidates.index(target)]
+            res = self.combinationSumHelp(candidates, target, len(candidates) - 1)
+            for item in res:
                 ret.append(item)
+        else:
+            # Take candidates[idx].
+            if idx >= 0 and target - candidates[idx] >= 0:
+                resTake = self.combinationSumHelp(candidates, target - candidates[idx], idx)
+                for item in resTake:
+                    item.insert(0, candidates[idx])
+                for item in resTake:
+                    ret.append(item)
 
-        
-        # Not take candidates[idx].
-        if idx - 1 >= 0:
-            resNotTake = self.combinationSumHelp(candidates, target, idx - 1)
-            for item in resNotTake:
-                ret.append(item)
+            # Not take candidates[idx].
+            if idx - 1 >= 0 and target > 0:
+                resNotTake = self.combinationSumHelp(candidates, target, idx - 1)
+                for item in resNotTake:
+                    ret.append(item)
 
         return ret
 
