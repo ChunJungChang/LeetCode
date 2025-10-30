6610d01a2e3cec3afb6a831a58d234fdbf75bb89
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2025-10-30 17:29:13 +0800

    Update DiscussSolution.c

--- Medium/138. Copy List with Random Pointer/DiscussSolution.c
+++ Medium/138. Copy List with Random Pointer/DiscussSolution.c
@@ -1,3 +1,6 @@
-Step 1: copy each node and append it to the original one
-Step 2: iterate the new list and fix the random pointers
-Step 3: separate the list
+/**
+ * Step 1: copy each node and append it to the original one
+ * Step 2: iterate the new list and fix the random pointers
+ * Step 3: separate the list
+ * https://bclin.tw/2022/06/06/leetcode-138/
+ */
