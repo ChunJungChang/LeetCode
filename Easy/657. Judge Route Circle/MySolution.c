053fbf0cccde05cf3e0372a9532f66cf1977d975
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2025-10-30 17:07:59 +0800

    Update MySolution.c

--- Easy/657. Judge Route Circle/MySolution.c
+++ Easy/657. Judge Route Circle/MySolution.c
@@ -1,3 +1,8 @@
+/**
+ * To call strlen function in each loop waste too much time.
+ * You should declaration a variable to store length of moves, it just calls strlen function once.
+ */
+
 bool judgeCircle(char* moves) {
     int len = strlen(moves), i = 0, x = 0, y = 0;
     
