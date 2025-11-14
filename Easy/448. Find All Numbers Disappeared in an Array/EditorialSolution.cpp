6374dd4eaabfae75c217aba0ae0aa6b031b60685
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2025-11-14 16:43:25 +0800

    Create EditorialSolution.cpp

---
+++ Easy/448. Find All Numbers Disappeared in an Array/EditorialSolution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ret;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int newIdx = abs(nums[i]) - 1;
+
+            if (nums[newIdx] > 0) {
+                nums[newIdx] = nums[newIdx] * -1;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                ret.push_back(i + 1);
+            }
+        }
+
+        return ret;
+    }
+};
