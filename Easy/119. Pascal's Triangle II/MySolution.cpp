294d3e003ed376a50acd58fc5d8cc2ed6972a393
ChunJungChang <34591635+ChunJungChang@users.noreply.github.com>
2025-11-25 16:08:17 +0800

    Update MySolution.cpp

--- Easy/119. Pascal's Triangle II/MySolution.cpp
+++ Easy/119. Pascal's Triangle II/MySolution.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> res, ret;
+        vector<int> res;
+        vector<int> ret;
 
         if (rowIndex == 0) {
-            ret.push_back(1);
-
-            return ret;
-        } else if (rowIndex == 1) {
-            ret.push_back(1);
             ret.push_back(1);
 
             return ret;
         }
 
         res = getRow(rowIndex - 1);
+
         ret.push_back(1);
-        for (int i = 0; i < res.size() - 1; i++) {
-            ret.push_back(res[i] + res[i + 1]);
+        for (auto it = res.begin(); it != res.end() - 1; it++) {
+            ret.push_back(*it + *(it + 1));
         }
         ret.push_back(1);
 
